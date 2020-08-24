8ccdb01939f5345e1db9c5d4b820fb8ee65a23b3
Radele Rose <radele.rose@gmail.com>
2020-08-24 06:48:57 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0 # if the number int is divisible by 3
     "FizzBuzz" # Go fizz
-  elsif int % 3 == 0 
+  elsif int % 3 == 0
     "Fizz"
   elsif int % 5 == 0
-    "Buzz" 
+    "Buzz"
   end
 end
 
